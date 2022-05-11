c836aabc79756b00f583c218e7cbdeb3cde1a47c
k0ntinuum <st088265@student.spbu.ru>
2022-05-11 14:46:12 +0300

    feat: add mouse input; finish output; add minor edits

--- src/application/controller.js
+++ src/application/controller.js
@@ -3,8 +3,11 @@ export default class Controller {
     this.keyboard = keyboard;
     this.output = output;
 
-    document.addEventListener('keydown', this.handleDownEvent.bind(this));
-    document.addEventListener('keyup', this.handleUpEvent.bind(this));
+    document.onkeydown = this.handleDownEvent.bind(this);
+    document.onkeyup = this.handleUpEvent.bind(this);
+
+    this.keyboard.container.node.onmousedown = this.handleMouseDownEvent.bind(this);
+    this.keyboard.container.node.onmouseup = this.handleMouseUpEvent.bind(this);
   }
 
   /**
@@ -18,28 +21,28 @@ export default class Controller {
     const key = this.keyboard.buttons.find((_key) => _key.code === event.code);
     if (!key) return;
     if (this.keyboard.keysPressed[key.code]) return;
-
+    if (!key.isFnKey) event.preventDefault();
     this.output.node.focus();
 
     const keyButton = key.button.node;
     keyButton.classList.add('active');
 
-    if (event.key === 'Tab' || event.key === 'Alt') event.preventDefault();
+    if (event.code === 'Tab' || event.code === 'Alt') event.preventDefault();
 
-    if (event.key === 'Shift') {
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
       this.keyboard.shiftKey = true;
       this.keyboard.switchCase();
     }
 
-    if (event.key === 'Control') this.keyboard.ctrlKey = true;
-    if (event.key === 'Alt') this.keyboard.altKey = true;
-    if ((event.key === 'Control' && this.keyboard.altKey)
-      || (event.key === 'Alt' && this.keyboard.ctrlKey)) this.keyboard.switchLayout();
+    if (event.code === 'ControlLeft' || event.code === 'ControlRight') this.keyboard.ctrlKey = true;
+    if (event.code === 'AltLeft' || event.code === 'AltRight') this.keyboard.altKey = true;
+    if (((event.code === 'ControlLeft' || event.code === 'ControlRight') && this.keyboard.altKey)
+      || ((event.code === 'AltLeft' || event.code === 'AltRight') && this.keyboard.ctrlKey)) this.keyboard.switchLayout();
 
-    if (event.key === 'CapsLock' && !this.keyboard.isCaps) {
+    if (event.code === 'CapsLock' && !this.keyboard.isCaps) {
       this.keyboard.isCaps = true;
       this.keyboard.switchCaps();
-    } else if (event.key === 'CapsLock' && this.keyboard.isCaps) {
+    } else if (event.code === 'CapsLock' && this.keyboard.isCaps) {
       this.keyboard.isCaps = false;
       this.keyboard.switchCaps();
       keyButton.classList.remove('active');
@@ -61,16 +64,55 @@ export default class Controller {
 
     this.output.node.focus();
 
+    if (!event.isTrusted || (!key.isFnKey && event.isTrusted)) this.createOutputStream(key, event);
+
     this.keyboard.reset(event.code, key);
 
-    if (event.key === 'Shift') {
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
       this.keyboard.shiftKey = false;
       this.keyboard.switchCase();
     }
 
-    if (event.key === 'Control') this.keyboard.ctrlKey = false;
-    if (event.key === 'Alt') this.keyboard.altKey = false;
+    if (event.code === 'ControlLeft' || event.code === 'ControlRight') this.keyboard.ctrlKey = false;
+    if (event.code === 'AltLeft' || event.code === 'AltRight') this.keyboard.altKey = false;
 
-    if (event.key !== 'CapsLock') key.button.node.classList.remove('active');
+    if (event.code !== 'CapsLock') key.button.node.classList.remove('active');
+  }
+
+  /**
+   *
+   * @param {MouseEvent} event
+   */
+
+  handleMouseDownEvent(event) {
+    const buttonNodes = this.keyboard.buttons.map((btn) => btn.letter.node);
+    if (!buttonNodes.includes(event.target)) return;
+
+    const { code } = this.keyboard.buttons[buttonNodes.indexOf(event.target)];
+
+    document.dispatchEvent(new KeyboardEvent('keydown', { code }));
+  }
+
+  /**
+   *
+   * @param {MouseEvent} event
+   */
+
+  handleMouseUpEvent(event) {
+    const buttonNodes = this.keyboard.buttons.map((btn) => btn.letter.node);
+    if (!buttonNodes.includes(event.target)) return;
+
+    const { code } = this.keyboard.buttons[buttonNodes.indexOf(event.target)];
+
+    document.dispatchEvent(new KeyboardEvent('keyup', { code }));
+  }
+
+  createOutputStream(key, event) {
+    const buttonNodes = this.keyboard.buttons.map((btn) => btn.letter.node);
+
+    const { isFnKey } = key;
+    const char = isFnKey ? '' : buttonNodes[this.keyboard.buttons.indexOf(key)];
+
+    this.output.createOutput(event.code, char.textContent);
   }
 }
