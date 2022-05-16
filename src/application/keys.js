a86c85cdd1f581d165c74243d5d4b28271c9dd71
k0ntinuum <st088265@student.spbu.ru>
2022-05-16 17:52:46 +0300

    fix: fix space bug

--- src/application/keys.js
+++ src/application/keys.js
@@ -304,7 +304,7 @@ export default {
     {
       code: 'Space',
       small: ' ',
-      shift: null,
+      shift: ' ',
     },
     {
       code: 'AltRight',
@@ -642,7 +642,7 @@ export default {
     {
       code: 'Space',
       small: ' ',
-      shift: null,
+      shift: ' ',
     },
     {
       code: 'AltRight',
