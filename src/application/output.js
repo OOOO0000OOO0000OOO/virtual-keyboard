432236e5f15e281a08c4f584088de6a8b67a0cca
k0ntinuum <st088265@student.spbu.ru>
2022-05-16 17:50:16 +0300

    feat: add text inserting/replacing

--- src/application/output.js
+++ src/application/output.js
@@ -11,62 +11,56 @@ export default class OutputField extends Control {
   }
 
   createOutput(code, char = '') {
-    let cursor = this.node.selectionStart;
+    this.cursor = this.node.selectionStart;
 
-    const before = this.node.value.slice(0, cursor);
-    const after = this.node.value.slice(cursor);
-
-    const width = Math.round((this.node.clientWidth - 20) / 10.9);
-    const valueLength = this.node.value.length;
+    const { selectionEnd } = this.node;
+    const before = this.node.value.slice(0, this.cursor);
+    const after = this.node.value.slice(this.cursor);
+    const afterSelection = this.node.value.slice(selectionEnd);
 
     this.outputHandlers = {
       Tab: () => {
-        cursor += 1;
+        this.cursor += 1;
         this.node.value = `${before}\t${after}`;
       },
       ArrowLeft: () => {
-        cursor = (cursor > 0 ? cursor - 1 : 0);
+        this.cursor = (this.cursor > 0 ? this.cursor - 1 : 0);
       },
       ArrowRight: () => {
-        cursor += 1;
+        this.cursor += 1;
       },
       ArrowUp: () => {
-        const rows = before.split('\n').reverse();
-        if (rows.length === 1 || rows[0].length > width) {
-          cursor = (cursor - width >= 0 ? cursor - width : 0);
-        } else {
-          cursor = rows[0].length > rows[1].length ? before.lastIndexOf('\n') : (before.lastIndexOf('\n') - rows[1].length + rows[0].length);
-        }
+        this.cursor += 1;
       },
       ArrowDown: () => {
-        const rows = after.split('\n');
-        if (rows.length === 1 || rows[0].length > width) {
-          cursor = (cursor + width <= valueLength ? cursor + width : valueLength);
-        } else {
-          const lineLeft = before.lastIndexOf('\n') === -1 ? cursor % width : cursor - before.lastIndexOf('\n');
-          cursor += after.indexOf('\n') + (lineLeft > rows[1].length ? rows[1].length + 1 : lineLeft);
-        }
+        this.cursor = (this.cursor > 0 ? this.cursor - 1 : 0);
       },
       Enter: () => {
         this.node.value = `${before}\n${after}`;
-        cursor += 1;
+        this.cursor += 1;
       },
       Delete: () => {
-        this.node.value = `${before}${after.slice(1)}`;
+        this.node.value = (this.cursor === selectionEnd)
+          ? `${before}${after.slice(1)}`
+          : `${before}${afterSelection}`;
       },
       Backspace: () => {
-        this.node.value = `${before.slice(0, -1)}${after}`;
-        cursor -= 1;
+        this.node.value = (this.cursor === selectionEnd)
+          ? `${before.slice(0, -1)}${after}`
+          : `${before}${afterSelection}`;
+        if (this.cursor === selectionEnd) this.cursor -= 1;
       },
       Key: () => {
-        cursor += 1;
-        this.node.value = `${before}${char}${after}`;
+        this.node.value = (this.cursor === selectionEnd)
+          ? `${before}${char}${after}`
+          : `${before}${char}${afterSelection}`;
+        this.cursor += 1;
       },
     };
 
     if (this.outputHandlers[code]) this.outputHandlers[code]();
     if (char) this.outputHandlers.Key();
 
-    this.node.setSelectionRange(cursor, cursor);
+    this.node.setSelectionRange(this.cursor, this.cursor);
   }
 }
